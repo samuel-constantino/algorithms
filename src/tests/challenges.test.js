f0807d149da484ebea91e9c709d04c806c5449eb
Samuel Constantino <samuelicapuidados@gmail.com>
2022-04-05 10:22:06 -0300

    feat: implementa testes do desafio 3

--- src/tests/challenges.test.js
+++ src/tests/challenges.test.js
@@ -1,4 +1,4 @@
-const { challenge1, challenge2 } = require('../challenges');
+const { challenge1, challenge2, challenge3 } = require('../challenges');
 
 describe('Challenge functions', function () {
   test('sum of multiples of 3 and 5', function () {
@@ -7,5 +7,9 @@ describe('Challenge functions', function () {
 
   test('Sum the even numbers of the Fibonacci sequence', function () {
     expect(challenge2(10)).toBe(44);
+  });
+
+  test('prime factors of 13195', function () {
+    expect(challenge3(13195)).toEqual([5, 7, 13, 29]);
   });
 });
